d158105251ce23a2370baaa74313a4118f5c0f91
Rafael Holanda do Amaral <rafael_holandamaral@hotmail.com>
2024-01-27 21:02:34 -0300

    feat: database connection

---
+++ public/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+use app\framework\database\Connection;
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__,2));
+$dotenv->load();
+
+$connection = Connection::getConnection();
+$query = $connection->query("select * from users");
+var_dump($query->fetchAll());
+
+
+routerExecute();
+
+
+
+?>
--- public/index.php
+++ public/index.php
@@ -3,6 +3,9 @@
 session_start();
 
 require '../vendor/autoload.php';
+require './bootstrap.php';
+
+// var_dump(dirname(__FILE__,2));
 
 
-routerExecute();
+
