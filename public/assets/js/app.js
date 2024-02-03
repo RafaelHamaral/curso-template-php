9bc4ea85185f0fbd640cb85e1b236a0acaf74c21
Rafael Holanda do Amaral <rafael_holandamaral@hotmail.com>
2024-02-03 10:51:18 -0300

    final: Webpack and babel support

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function getStarWarsPlanets(){
     try {
-        const {data} = await axios.get('https://swapi.dev/api');
+        const {data} = await axios.get('https://swapi.dev/api/planets');
         console.log(data);
     } catch (error) {
        console.log(error.response);
